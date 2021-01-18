f9bbc54875b4c58ccb64fcc243750afec3f91ac9
benoitdemaegdt <benoit.demaegdt@gmail.com>
2021-01-18 08:31:44 +0100

    setup demo

---
+++ components/global/CustomComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>This article has {{ comments }} comments</p>
+</template>
+
+<script>
+export default {
+  name: 'CustomComponent',
+  props: {
+    comments: { type: Number, default: 0 },
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ content/articles/my-first-blog-post.md
@@ -0,0 +1,8 @@
+---
+comments: 0
+---
+# My first blog post
+
+Welcome to my first blog post using content module
+
+<custom-component :comments="comments"><custom-component/>
